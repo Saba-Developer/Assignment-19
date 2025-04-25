e677353ab7e498954b14c4e426e9fc0b96ee4c45
Saba-Developer <sabamadina87@gmail.com>
2025-04-25 12:08:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Enter the 4 digit number:")
+let num_1=parseInt(prompt("Enetr 1st Number : "));
+let num_2=parseInt(prompt("Enetr 2nd Number : "));
+let num_3=parseInt(prompt("Enetr 3rd Number : "));
+let num_4=parseInt(prompt("Enetr 4th Number : "));
+console.log(`Reverse Pattern: ${num_4} , ${num_3} , ${num_2} , ${num_1}`);
